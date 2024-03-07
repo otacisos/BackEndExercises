764cd017cd6a3621ea892319f5e082cac3cec1a3
otaciso silva <otacisods@gmail.com>
2024-03-07 19:17:04 -0300

    Lendo arquivos com métodos assíncronos

--- 03WebNodejs/LeituraEscritaArquivos/my-node-project/readFileSync.js
+++ 03WebNodejs/LeituraEscritaArquivos/my-node-project/readFileSync.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const nomeDoArquivo = 'meu-arquivo.txt';
-
-try {
-  const data = fs.readFileSync(nomeDoArquivo, 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error(`Erro ao ler o arquivo: ${err.path}`);
-  console.log(err);
+async function  main () {
+  try {
+    const data = await fs.readFile('./meu-arquivo', 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.error(`Erro no sistema ${err.message}`);
+  }
 }
+
+main()
